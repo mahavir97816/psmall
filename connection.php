3e58a28d7759f3e0c425da5c4d1282a15958a974
mahavir97816 <40319689+mahavir97816@users.noreply.github.com>
2021-03-04 22:56:42 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "ps_mall";
+$servername = "remotemysql.com";
+$username = "P6BTlJ0kEA";
+$password = "EL4IIoyNNL";
+$dbname = "P6BTlJ0kEA";
 
 $conn = mysqli_connect($servername,$username,$password,$dbname);
 
